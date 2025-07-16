226f398858a043e6da1362a832f2246f4c7b50fc
Rajesh S Nair <67047853+vemarahub@users.noreply.github.com>
2025-07-16 17:29:23 +0200

    Update user-data.sh

--- runner-infrastructure/user-data.sh
+++ runner-infrastructure/user-data.sh
@@ -10,10 +10,10 @@ apt-get install -y curl git unzip wget jq awscli
 
 # Install Terraform
 TERRAFORM_VERSION="1.6.6"
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/$${TERRAFORM_VERSION}/terraform_$${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform_$${TERRAFORM_VERSION}_linux_amd64.zip
 mv terraform /usr/local/bin/
-rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+rm terraform_$${TERRAFORM_VERSION}_linux_amd64.zip
 
 # Install Node.js (for some GitHub Actions)
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
@@ -25,9 +25,9 @@ cd /home/ubuntu/actions-runner
 
 # Download GitHub runner
 RUNNER_VERSION="2.311.0"
-curl -o actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
-tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
-rm actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
+curl -o actions-runner-linux-x64-$${RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v$${RUNNER_VERSION}/actions-runner-linux-x64-$${RUNNER_VERSION}.tar.gz
+tar xzf ./actions-runner-linux-x64-$${RUNNER_VERSION}.tar.gz
+rm actions-runner-linux-x64-$${RUNNER_VERSION}.tar.gz
 
 # Set ownership
 chown -R ubuntu:ubuntu /home/ubuntu/actions-runner
@@ -47,4 +47,4 @@ cd /home/ubuntu/actions-runner
 systemctl status actions.runner.ubuntu.* || true
 
 # Log completion
-echo "GitHub runner setup completed at $(date)" >> /var/log/runner-setup.log
+echo "GitHub runner setup completed at $$(date)" >> /var/log/runner-setup.log

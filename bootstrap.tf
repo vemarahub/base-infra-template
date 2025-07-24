115eb422ac1b030203f8b78fff95ce1b0191eeaf
Rajesh Nair <vemarahub@gmail.com>
2025-07-24 19:25:56 +0200

    s3 commit

--- bootstrap.tf
+++ bootstrap.tf
@@ -54,7 +54,9 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
   name           = "terraform-state-lock-${local.clean_project_name}"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
-
+  lifecycle {
+    prevent_destroy = true
+  }
   attribute {
     name = "LockID"
     type = "S"
